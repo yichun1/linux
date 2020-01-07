d455f4252b4abec41355559423bc84faa3f998eb
Yuen <satoshi@qq.com>
2020-01-07 16:14:51 +0800

    add function(void process_trans) into myweb

--- myweb.c
+++ myweb.c
@@ -20,7 +20,8 @@ void parse_static_uri(char *uri,char *filename);
 void parse_dynamic_uri(char *uri,char *filename,char *cgiargs);
 void feed_static(int fd,char *filename,int filesize);
 void get_filetype(char *filename,char *filetype);
-void feed_dynamic(int fd,char *filename,char *cgiargs);
+void feed_dynamic_get(int fd,char *filename,char *cgiargs);
+void feed_dynamic_post(int fd,char *filename);
 void error_request(int fd,char *cause,char *errnum,char *shortmsg,char *description);
 
 int main(int argc,char **argv){
@@ -49,3 +50,73 @@ void *serve_client(void *vargp){
 	close(con_socks);
 	return NULL;
 }
+
+void process_trans(int fd){
+	int static_flag,cgi=0;
+	struct stat sbuf;
+	char buf[1024],method[1024],uri[1024],version[1024];
+	char filename[1024],cgiargs[1024];
+	rio_t rio;
+
+	rio_readinitb(&rio,fd);
+	rio_readlineb(&rio,buf,1024);
+	sscanf(buf,"%s %s %s",method,uri,version);
+	if(strcasecmp(method,"GET")&&strcasecmp(method,"POST")){
+		unimplemented(fd);
+		return;
+	}
+
+	read_requesthdrs(&rio);
+
+	static_flag=is_static(uri);
+	if(static_flag)
+		parse_static_uri(uri,filename);
+	else
+		parse_dynamic_uri(uri,filename,cgiargs);
+
+	if(strcasecmp(method,"POST")==0){
+		if(stat(filename,&sbuf)<0){
+			error_request(fd,filename,"404","Not found","myweb could not find this file");
+			return;
+		}
+
+		if(static_flag){
+			if(!S_ISREG(sbuf.st_mode))||!(S_IRUSR&sbuf.st_mode)){
+				error_request(fd,filename,"403","Forbidden","myweb is not permtted to read the file");
+				return;
+			}
+			feed_static(fd,filename,sbuf.st_size);
+		}
+		else{
+			if(!(S_ISREG(sbuf.st_mode))||!(S_IXUSR&sbuf.st_mode)){
+				error_request(fd,filename,"403","Forbidden","myweb could not run the CGI program");
+				return;
+			}
+			feed_dynamic_post(fd,filename,cgiargs);
+		}
+	}
+
+	if(strcasecmp(method,"GET")==0){
+
+		if(stat(filename,&sbuf)<0){
+			error_request(fd,filename,"404","Not found","myweb could not find this file");
+			return;
+		}
+
+		if(static_flag){
+			if(!S_ISREG(sbuf.st_mode))||!(S_IRUSR&sbuf.st_mode)){
+				error_request(fd,filename,"403","Forbidden","myweb is not permtted to read the file");
+				return;
+			}
+			feed_static(fd,filename,sbuf.st_size);
+		}
+		else{
+			if(!(S_ISREG(sbuf.st_mode))||!(S_IXUSR&sbuf.st_mode)){
+				error_request(fd,filename,"403","Forbidden","myweb could not run the CGI program");
+				return;
+			}
+			feed_dynamic_get(fd,filename,cgiargs);
+		}
+	}
+}
+
